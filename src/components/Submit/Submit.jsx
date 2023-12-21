96c1fd38c56fafdab8b28ee658478a8e24fff3d0
zarif <zarif@icloud.com>
2023-12-21 16:09:25 +0500

    test commit

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
 import styles from "./Submit.module.scss";
 
-
 function Submit({ closeModal }) {
   useEffect(() => {
-     window.roistatVisitCallback = function (visitId) {
+    window.roistatVisitCallback = function (visitId) {
       window.addEventListener('b24:form:init', (event) => {
         let form = event.detail.object;
-        form.setProperty("roistatID", visitId);
-        form.setProperty("promotion", "trial account");
-        
+        form.setFields({
+          "roistatID": visitId,
+          "promotion": "trial account"
+        });
       });
-
 
       (function (w, d, u) {
         var s = d.createElement('script');
@@ -19,39 +18,48 @@ function Submit({ closeModal }) {
         s.src = u + '?' + (Date.now() / 180000 | 0);
         var h = d.getElementsByTagName('script')[0];
         h.parentNode.insertBefore(s, h);
-      })(window, document, 'https://cdn-ru.bitrix24.ru/b20141368/crm/form/loader_126.js');
+      })(window, document, 'https://cdn-ru.bitrix24.ru/b27952308/crm/form/loader_3.js');
 
       window.roistatVisitCallback();
-    }
-  })
-   
-  return (
-        <div className={styles.submit}>
-      <div className={styles.modalOverlay}>
-        <div className={styles.modal}>
-            <div className={styles.navBox}>
-                <img className={styles.img} src="./images/logo.webp" alt="logo" loading="lazy" />
-                <div className={styles.line}></div>
-                <ul className={styles.links}>
-                    <li>Global</li>
-                    <li>Service</li>   
-                    <li>Parther</li>
-                </ul>
-                <div className={styles.line}></div>
-                <img className={styles.img}  src="./images/logo2.webp" alt="logo2" loading="lazy"/>
-            </div>
-            <img className={styles.close} onClick={closeModal} src="./images/close.svg" alt="close" />
-            <script data-b24-form="click/126/w8kvl2" data-skip-moving="true">
+    };
+  }, [closeModal]);
 
+  return (
+    <div className={styles.submit}>
+      <div className={styles.modalOverlay} onClick={closeModal}>
+        <div className={styles.modal}>
+
+          {/* ваша форма  */}
+
+        {/* <script data-b24-form="click/126/w8kvl2" data-skip-moving="true">
+  {
+    window.roistatVisitCallback = function (visitId) {
+        window.addEventListener('b24:form:init', (event) => {
+            let form = event.detail.object;
+            form.setProperty("roistatID", visitId);
+            form.setProperty("promotion", );
+        });
+        (function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b20141368/crm/form/loader_126.js');
+    }
+  }
+</script> */}
+
+{/* Форма взятая с битрикс  */}
+
+<script data-b24-form="inline/3/l2r8q8" data-skip-moving="true">
+{(function(w,d,u){
+var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+})(window,document,'https://cdn-ru.bitrix24.ru/b27952308/crm/form/loader_3.js')}
 </script>
+
+
 
         </div>
       </div>
     </div>
   );
-
-  }
-
+}
 
 export default Submit;
 
