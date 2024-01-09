6af5b555cf8d1110f70310383564a112f0a99322
zarif <zarif@icloud.com>
2024-01-09 22:06:01 +0500

    test commit

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -7,7 +7,7 @@ function Submit({ closeModal }) {
       <div className={styles.modalOverlay} onClick={closeModal}>
         <div className={styles.modal}>
           {/* ваша форма  */}
-          <script data-b24-form="click/126/w8kvl2" data-skip-moving="true">
+          {/* <script data-b24-form="click/126/w8kvl2" data-skip-moving="true">
             {
 window.roistatVisitCallback = function (visitId) {
     window.addEventListener('b24:form:init', (event) => {
@@ -18,11 +18,11 @@ window.roistatVisitCallback = function (visitId) {
     (function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b20141368/crm/form/loader_126.js')
 }
 }
-</script>
+</script> */}
       {/* форма которую я взял сам с Битриск24 */}
-      {/* <script data-b24-form="inline/3/v9akzs" data-skip-moving="true">
+      <script data-b24-form="inline/3/v9akzs" data-skip-moving="true">
         {(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b27951100/crm/form/loader_3.js')}
-        </script> */}
+        </script>
 
         </div>
       </div>
